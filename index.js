01f01907d91449f19f74c186dab730617b6a7a8e
ImACouch <141815028+ImACouch@users.noreply.github.com>
2024-03-02 15:53:26 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const express = require(express)
+const express = require("express")
 const app = express()
+
+const PORT = process.env.PORT | 5000;
+
+app.get('/',(req,res)=>{
+    res.json({message:"Hello, from backend!"})
+});
 
 app.listen(PORT,()=>{
     console.log(`Server is lisening on ${PORT}`)
