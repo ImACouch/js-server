9493651ba1cb4ae6fbaa6c6d0eba4c4e7b0668bb
ImACouch <141815028+ImACouch@users.noreply.github.com>
2024-03-02 15:34:53 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,6 @@
-const WebSocket = require("ws");
-var number=1;
-var CLIENTS=[];
+const express = require(express)
+const app = express()
 
-
-//make/start server
-var wss = new WebSocket.Server({ port: 8082 });
-wss.broadcast = function(data) {
-    //sends data to all connecshons
-    wss.clients.forEach(function(client) {
-      
-    client.send(data)
-      
-    })
-  }
-
-//lison for connecshions
-wss.on("connection", ws => {
-    console.log("New client connected!");
-    ws.send(number);
-
-    ws.on("message", data => {
-        //console.log('client has sent us: '+String(data));
-        number+=parseInt(data);
-        console.log(parseInt(data));
-        //ws.send(number);
-        wss.broadcast(number);
-    });
-
-    ws.on("close", () => {
-        console.log("client disconected!");
-    });
-
+app.listen(PORT,()=>{
+    console.log(`Server is lisening on ${PORT}`)
 });
