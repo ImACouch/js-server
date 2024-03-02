5c1456beda5cf66cecbf7e9ed7198d4e42528bc0
ImACouch <141815028+ImACouch@users.noreply.github.com>
2024-03-02 15:21:13 -0800

    Update package.js

--- package.js
+++ package.js
@@ -1,1 +1,20 @@
-
+{
+    "name": "js-server",
+    "version": "1.0.0",
+    "description": "",
+    "scripts": {
+        "build": "echo \"No build specified\" && exit 1",
+        "test": "echo \"No test specified\" && exit 1",
+        "start": "node index.js"
+    },
+    "repository": {
+        "type": "git",
+        "url": "git+https://github.com/ImACouch/js-server.git"
+    },
+    "author": "",
+    "license": "ISC",
+    "bugs": {
+        "url": "https://github.com/ImACouch/js-server/issues"
+    },
+    "homepage": "https://github.com/ImACouch/js-server#readme"
+}
